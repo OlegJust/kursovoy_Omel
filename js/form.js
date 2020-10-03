78431fa1e7bf0376e0f70c130d95a2dc13efb6ff
OlegJust <olegpodgo@gmail.com>
2020-10-03 18:32:57 +0300

    пп

--- js/form.js
+++ js/form.js
@@ -10,7 +10,8 @@ butt.onclick = function () {
 	let w = document.getElementById('w').value;
 	let Mco = document.getElementById('Mco').value;
 	let Mcm = document.getElementById('Mcm').value;
-	
+	let Kz = document.getElementById('Kz').value;
+	let Mekv = 0
 	value('#input_t1', t1)
 	value('#input_t2', t2)
 	value('#input_t3', t3)
@@ -21,10 +22,13 @@ butt.onclick = function () {
 	value('#input_w', w)
 	value('#input_Mco', Mco)
 	value('#input_Mcm', Mcm)
+	value('#input_Kz', Kz)
+	value('#input_Mekv', Mekv)
 	tras1(ter, ter1, ter2) // Для того чтобы после перезаписование значений корень закрывал все облость 
 	tras1(ter, ter1, ter2) // это просто ржака, у меня функц с третего раза срабатывает, что я сделал правильно запустил 3 раза )
 	tras1(ter, ter1, ter2)
 	funMekv()
+	funPnnp()
 };
 // ---------------------
 // корень
@@ -41,7 +45,7 @@ function tras1(name, name1, name2) {
 		let w = cars[ind]
 		let bloc = name[ind]
 		let form = name2[ind]
-		form.style.width = String(w  + 110) + "px"
+		form.style.width = String(w + 110) + "px"
 		bloc.style.width = String(w + 20) + "px"
 	}
 }
@@ -63,6 +67,12 @@ function funMekv() {
 	const numerator = Math.pow(+Mco.value, 2) * (+t5.value + +t7.value) + Math.pow(+Mcm.value, 2) * +t6.value
 	const denominator = +t1.value + +t2.value + +t3.value + +t4.value
 	// document.querySelector('.Mekv__answer').innerHTML = Math.sqrt(numerator/denominator)
-	document.querySelector('.Mekv__answer').innerHTML = "= " + String(Math.sqrt(numerator/denominator).toFixed(1) + " Нм")
+	Mekv = +(Math.sqrt(numerator / denominator).toFixed(1))
+	document.querySelector('.Mekv__answer').innerHTML = "= " + String(Math.sqrt(numerator / denominator).toFixed(1) + " Нм")
+}
+//-------------------------
+// Находим Рнпр
+function funPnnp() {
+	document.querySelector('.Pnnp__answer').innerHTML = "= " + String((+Kz.value * Mekv * +w.value * Math.pow(10, -3)).toFixed(1) + " кВт")
 }
 //-------------------------
