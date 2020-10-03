2ad7200132793f11a5c84fcc3623163abd5f0628
OlegJust <olegpodgo@gmail.com>
2020-10-03 11:01:06 +0300

    ВВОД

--- js/form.js
+++ js/form.js
@@ -10,6 +10,7 @@ butt.onclick = function () {
 	let w = document.getElementById('w').value;
 	let Mco = document.getElementById('Mco').value;
 	let Mcm = document.getElementById('Mcm').value;
+	
 	value('#input_t1', t1)
 	value('#input_t2', t2)
 	value('#input_t3', t3)
@@ -20,16 +21,17 @@ butt.onclick = function () {
 	value('#input_w', w)
 	value('#input_Mco', Mco)
 	value('#input_Mcm', Mcm)
-	tras1(ter,ter1,ter2) // Для того чтобы после перезаписование значений корень закрывал все облость 
-	tras1(ter,ter1,ter2) // это просто ржака, у меня функц с третего раза срабатывает, что я сделал правильно запустил 3 раза )
-	tras1(ter,ter1,ter2)
+	tras1(ter, ter1, ter2) // Для того чтобы после перезаписование значений корень закрывал все облость 
+	tras1(ter, ter1, ter2) // это просто ржака, у меня функц с третего раза срабатывает, что я сделал правильно запустил 3 раза )
+	tras1(ter, ter1, ter2)
+	funMekv()
 };
 // ---------------------
 // корень
 const ter = document.querySelectorAll('.block3')
 const ter2 = document.querySelectorAll('.Mekv__formula')
 const ter1 = document.querySelectorAll('.Mekv__formula_bottom')
-function tras1(name,name1,name2) {
+function tras1(name, name1, name2) {
 	const cars = []
 	for (let car of name1) {
 		let lot = car.clientWidth
@@ -39,21 +41,28 @@ function tras1(name,name1,name2) {
 		let w = cars[ind]
 		let bloc = name[ind]
 		let form = name2[ind]
-		form.style.width = String(w + 20 + 110) + "px"
+		form.style.width = String(w  + 110) + "px"
 		bloc.style.width = String(w + 20) + "px"
 	}
 }
-tras1(ter,ter1,ter2)
+tras1(ter, ter1, ter2)
 // ---------------------
 // Здесь я сделал так чтобы одно значение выводилась везде 
-function value(where, what){
+function value(where, what) {
 	const mas = document.querySelectorAll(where)
-	if (what !== ""){
+	if (what !== "") {
 		for (let ind = 0; ind < mas.length; ind++) {
 			let w = mas[ind]
-			name34 = w.innerHTML
 			w.innerHTML = what
 		}
 	}
+}
+//-------------------------
+// Находим Mэкв
+function funMekv() {
+	const numerator = Math.pow(+Mco.value, 2) * (+t5.value + +t7.value) + Math.pow(+Mcm.value, 2) * +t6.value
+	const denominator = +t1.value + +t2.value + +t3.value + +t4.value
+	// document.querySelector('.Mekv__answer').innerHTML = Math.sqrt(numerator/denominator)
+	document.querySelector('.Mekv__answer').innerHTML = "= " + String(Math.sqrt(numerator/denominator).toFixed(1) + " Нм")
 }
 //-------------------------
