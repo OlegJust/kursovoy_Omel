17efbd8f16dae5f21318d2aea4bef754d703df30
OlegJust <olegpodgo@gmail.com>
2020-11-11 20:49:18 +0300

    djn

--- js/form.js
+++ js/form.js
@@ -32,7 +32,7 @@ butt.onclick = function () {
 	funPnnp()
 	// n
 	document.querySelector('.n__answer').innerHTML = "= " + String((w * 9.55).toFixed(1))
-	//EngineData()
+	EngineData()
 };
 // ---------------------
 // корень
@@ -81,11 +81,11 @@ function funPnnp() {
 }
 //-------------------------
 // вставление html кода 
-// function EngineData () {
-// 	if (document.querySelector('.EngineData').innerHTML === "") {
-// 		document.querySelector('.EngineData').insertAdjacentHTML('beforeend', '<div class="engine">Даные Двигателя</div><div class="engine_selection"><div class="value"><div class="value__text"><span>U</span>1ном =</div><input tabindex="1" value="380" type="number" name="U1n" id="U1n" class="Un input"></div><div class="value"><div class="value__text"><span>Р</span>ном =</div><input tabindex="2" value="19" type="number" name="Pn" id="Pn" class="Pn input"></div><div class="value"><div class="value__text"><span>n</span>ном =</div><input tabindex="3" value="0.5" type="number" name="nn" id="nn" class="nn input"></div><div class="value"><div class="value__text"><span>I</span>1н =</div><input tabindex="4" value="20" type="number" name="I1n" id="I1n" class="I1n input"></div><div class="value"><div class="value__text"><span>η</span> =</div><input tabindex="5" value="1.8" type="number" name="_n" id="_n" class="_n input"></div><div class="value"><div class="value__text"><span>cosφ</span> =</div><input tabindex="6" value="16.6" type="number" name="cosf" id="cosf" class="cosf input"></div><div class="value"><div class="value__text"><span>λ</span> =</div><input tabindex="7" value="1.62" type="number" name="Lambda" id="Lambda" class="Lambda input"></div><div class="value"><div class="value__text"><span>U</span>2н =</div><input tabindex="8" value="62" type="number" name="U2n" id="U2n" class="U2n input"></div><div class="value"><div class="value__text"><span>GD</span><div class="2"><div class="power">2</div></div>g =</div><input tabindex="9" value="220" type="number" name="GDg" id="GDg" class="GDg input"></div></div><div class="buttonRun"><div class="buttonRunBeg"><input id="butt1" class="butt" type="button" value="Run" /></div></div>')
-// 	}
-// }
+function EngineData () {
+	if (document.querySelector('.EngineData').innerHTML === "") {
+		document.querySelector('.EngineData').insertAdjacentHTML('beforeend', '<div class="engine">Даные Двигателя</div><div class="engine_selection"><div class="value"><div class="value__text"><span>U</span>1ном =</div><input tabindex="1" value="380" type="number" name="U1n" id="U1n" class="Un input"></div><div class="value"><div class="value__text"><span>Р</span>ном =</div><input tabindex="2" value="100" type="number" name="Pn" id="Pn" class="Pn input"></div><div class="value"><div class="value__text"><span>n</span>ном =</div><input tabindex="3" value="570" type="number" name="nn" id="nn" class="nn input"></div><div class="value"><div class="value__text"><span>I</span>1н =</div><input tabindex="4" value="208" type="number"name="I1n" id="I1n" class="I1n input"></div><div class="value"><div class="value__text"><span>η</span> =</div><input tabindex="5" value="90.2" type="number" name="_n" id="_n" class="_n input"></div><div class="value"><div class="value__text"><span>cosφ</span> =</div><input tabindex="6" value="0.81" type="number" name="cosf" id="cosf" class="cosf input"></div><div class="value"><div class="value__text"><span>λ</span> =</div><input tabindex="7" value="1.85" type="number" name="Lambda" id="Lambda" class="Lambda input"></div><div class="value"><div class="value__text"><span>U</span>2н =</div><input tabindex="8" value="62" type="number" name="U2n" id="U2n" class="U2n input"></div><div class="value"><div class="value__text"><span>GD</span><div class="2"><div class="power">2</div></div>g =</div><input tabindex="9" value="440" type="number" name="GDg" id="GDg" class="GDg input"></div></div><div class="buttonRun"><div class="buttonRunBeg"><input id="butt1" class="butt" type="button" value="Run" /></div></div><div class="Jg">Jg =<div class="Jg__formula"><div class="numerator"><div class="numerator__flex">GD<div class="2"><div class="power">2</div></div>g</div></div><div class="denominator">40</div></div>=<div class="Jg__decision"><div class="numerator" id="input_GDg"><div class="numerator__flex">GD<div class="2"><div class="power">2</div></div>g</div></div><div class="denominator">40</div></div><div class="Jg__answer"></div></div><div class="Jw"><div class="Jw__formula">J<span class="Jw__formula_w">w</span></div><div class="Jw__decision">= 1.4 × Jg</div><div class="Jw__decision">= 1.4 × <span id="input_Jg">Jg</span></div><div class="Jw__answer"></div></div><div class="engine1">Короче мне стало дальше лень это все оформлять</div><div class="answer">Ответы: <span class="answer__answer"></span></div><div class="check">1. Проверка: <span class="check__check1"></span></div><div class="check">2. Проверка: <span class="check__check2"></span></div>')
+	}
+}
 
 //-------------------------
 // 2 run
@@ -114,7 +114,9 @@ butt1.onclick = function () {
 	value('#input_GDg', GDg)
 	jg()
 	jw()
-	answer()
+	let gry = answer()
+	console.log(gry)
+	check(gry)
 };
 // Jg
 function jg() {
@@ -131,11 +133,36 @@ function jw() {
 }
 answer
 function answer() {
-	let Wn = (+nn.value / 9.55).toFixed(1) // номинальная омега
+	let Wn = +(+nn.value / 9.55).toFixed(1) // номинальная омега
 	let Mp = Math.round(+Mco.value + Jw * (Wn / +t1.value)) // Мп
 	let Mt = Math.round(+Mco.value - Jw * (Wn / +t3.value))
 	let Mn = Math.round((Math.pow(10, 3) * +Pn.value) / Wn)
 	let Mk = Math.round(+Lambda.value * Mn)
 	let _Mekv = Math.round(Math.sqrt((Math.pow(Mp, 2) * +t1.value + Math.pow(+Mco.value, 2) * (+t5.value + +t7.value - +t1.value - +t3.value) + Math.pow(+Mcm.value, 2) * +t6.value + (-Math.pow(Mt, 2) * +t3.value))/(+t1.value + +t2.value + +t3.value + +t4.value)))
 	console.log(`Jg: ${Jg}; Jw: ${Jw}; Wн: ${Wn}; Mп: ${Mp}; Мт: ${Mt}; Мн: ${Mn}; Мк: ${Mk}; М*экв: ${_Mekv};`)
+	document.querySelector('.answer__answer').innerHTML = `Jg: ${Jg}; Jw: ${Jw}; Wн: ${Wn}; Mп: ${Mp}; Мт: ${Mt}; Мн: ${Mn}; Мк: ${Mk}; М*экв: ${_Mekv};`
+	let gry = {
+		Wn:Wn,
+		Mp:Mp,
+		Mt:Mt,
+		Mn:Mn,
+		Mk:Mk,
+		_Mekv:_Mekv
+	}
+	return gry
+}
+function check(gry) {
+	let check1 = (gry.Mp / gry.Mk).toFixed(2)
+	let check2 = gry.Mn >= gry._Mekv
+	if ((check1 >= 0.55) && (check1 <= 0.65)) {
+		document.querySelector('.check__check1').innerHTML = "Выполнена"
+		console.log("ok")
+	} else {
+		document.querySelector('.check__check1').innerHTML = "Не выполнена"
+	}
+	if (check2) {
+		document.querySelector('.check__check2').innerHTML = "Выполнена"
+	} else {
+		document.querySelector('.check__check2').innerHTML = "Не выполнена"
+	}
 }
